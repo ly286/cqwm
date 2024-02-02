e5ea99631714271c4efae453ff07e14f64db249b
ly286 <dlylove286@gmail.com>
2024-02-02 15:12:17 +0800

    da3.1
    启用禁用员工账号

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -18,12 +18,25 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
-    //插入员工数据
+    /**
+     * 插入员工数据
+     * @param employee
+     */
     @Insert("insert into employee(username,name,password,phone,sex,id_number,create_time,update_time,create_user,update_user,status) " +
             "values" +
             "(#{username},#{name},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})")
     void insert(Employee employee);
 
-    //分页查询
+    /**
+     * 分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     * 根据主键动态修改属性
+     * @param employee
+     */
+    void update(Employee employee);
 }
