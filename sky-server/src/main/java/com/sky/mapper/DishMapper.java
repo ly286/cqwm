a11e64172aca9885687f6349db1cc1ff929e4afe
ly286 <dlylove286@gmail.com>
2024-02-23 16:24:35 +0800

    day7
    通过redis设置店铺营业状态

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,7 +1,5 @@
 package com.sky.mapper;
 
-<<<<<<< Updated upstream
-=======
 import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
 import com.sky.dto.DishPageQueryDTO;
@@ -9,7 +7,6 @@ import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
 import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Delete;
->>>>>>> Stashed changes
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -24,8 +21,7 @@ public interface DishMapper {
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
 
-<<<<<<< Updated upstream
-=======
+
     /**
      * 插入菜品数据
      *
@@ -63,5 +59,4 @@ public interface DishMapper {
      */
     @AutoFill(value = OperationType.UPDATE)
     void updateByPrimaryKey(Dish dish);
->>>>>>> Stashed changes
 }
